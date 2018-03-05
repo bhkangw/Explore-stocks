f765a6ed519e0c7115d18af032829df50ba5923e
Brian Kang <bhkangw@gmail.com>
2018-03-04 22:56:22 -0800

    Styled component view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import axios from 'axios'
+import axios from 'axios';
+var NumberFormat = require('react-number-format');
 
 class App extends Component {
   constructor(props) {
@@ -41,18 +42,20 @@ class App extends Component {
           <h2>Explore Consulting Stock Ticker</h2>
         </div>
 
-        <div className='container'>
+        <div className='jumbotron container'>
           <form onSubmit={this.handleSubmit.bind(this)}>
             <div>
-              <label>Stock:</label> <br/>
-              <input className='form-control' type='text' ref='stock' />
-              <input className='btn btn-success' type='submit' value='Submit' />
+              <p>Enter a stock symbol to find current price (USD):</p>
+              <input className='form-control' type='text' ref='stock' placeholder='Enter mutiple symbols separated by commas' /> <br />
+              <input className='btn btn-success pull-right' type='submit' value='Submit' />
             </div>
           </form>
+        </div>
+        <div className='container'>
           {Object.keys(this.state.stocks).map((key, index) => (
             <div id='stocks-container'>
-              {this.state.stocks[key]['1. symbol']}
-              {this.state.stocks[key]['2. price']}
+              <span className='left'>{this.state.stocks[key]['1. symbol']}</span>
+              <span className='right'><NumberFormat value={this.state.stocks[key]['2. price']} displayType={'text'} decimalScale={2} thousandSeparator={true} prefix={'$'} /></span>
             </div>
           ))}
         </div>
